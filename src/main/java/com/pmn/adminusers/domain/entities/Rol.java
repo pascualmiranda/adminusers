c0baf4615038ab40e4efe0e257ab68ae84c7011f
Pascual Miranda <pascualmn@gmail.com>
2023-11-19 19:44:19 -0400

    Mapeo de las 4 tablas a entities

---
+++ src/main/java/com/pmn/adminusers/domain/entities/Rol.java
@@ -0,0 +1,33 @@
+package com.pmn.adminusers.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="rol")
+public class Rol {
+    @Id
+    @SequenceGenerator(name = "rol_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "rol_sequence")
+    private int id;
+    private String name;
+
+    public Rol(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
