4b8e3d6f48422a171f73170746962ff89931b1b2
Pascual Miranda <pascualmn@gmail.com>
2023-11-19 12:31:20 -0400

    Primer commit

---
+++ src/main/java/com/pmn/adminusers/AdminusersApplication.java
@@ -0,0 +1,13 @@
+package com.pmn.adminusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmn/adminusers/AdminusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmn.adminusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
